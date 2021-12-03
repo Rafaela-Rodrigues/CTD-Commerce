8394a953a8b6f42b438c5b663bb0b7f8e3eef36c
DomBarbosa8 <mobarbosa.phoenix@gmail.com>
2021-12-02 21:49:39 -0300

    atualizou as dependências

---
+++ src/components/renan/carousel.jsx
@@ -0,0 +1,16 @@
+<Carousel>
+    <Carousel.Item>
+        <Row>
+            <Col md={3} sm={6}>
+                <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
+            </Col> <Col md={3} sm={6} className="d-none d-sm-block">
+                <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
+            </Col>
+            <Col md={3} sm={6} className="d-none d-sm-block">
+                <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
+            </Col> <Col md={3} sm={6} className="d-none d-sm-block">
+                <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
+            </Col>
+        </Row>
+    </Carousel.Item>
+</Carousel>
