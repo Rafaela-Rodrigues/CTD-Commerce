2b9126a303e5b1626e7252690705158c620240e6
abne <ba.alqueres@gmail.com>
2021-12-07 20:56:57 -0300

    carousel

--- src/components/renan/carousel.jsx
+++ src/components/renan/carousel.jsx
@@ -3,12 +3,14 @@
         <Row>
             <Col md={3} sm={6}>
                 <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
-            </Col> <Col md={3} sm={6} className="d-none d-sm-block">
+            </Col> 
+            <Col md={3} sm={6} className="d-none d-sm-block">
                 <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
             </Col>
             <Col md={3} sm={6} className="d-none d-sm-block">
                 <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
-            </Col> <Col md={3} sm={6} className="d-none d-sm-block">
+            </Col> 
+            <Col md={3} sm={6} className="d-none d-sm-block">
                 <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
             </Col>
         </Row>
