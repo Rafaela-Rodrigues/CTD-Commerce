cc87a1267a4d61806399ad3caeba012f8b592a27
Murilo Cesar "Muca <85465611+MuriloMuca@users.noreply.github.com>
2021-12-07 13:54:19 -0300

    About Us
    Atualização

--- src/components/pages/AboutUs/index.js
+++ src/components/pages/AboutUs/index.js
@@ -4,7 +4,8 @@ import React from "react";
 const AboutUs = () => {
     return (
         <>
-// foto do perfil
+
+// Card foto Aluno 1
             <div className = "profileTeam">
                 <form method = "">
                     <div className = "row">
@@ -15,7 +16,8 @@ const AboutUs = () => {
                             <div className = "bodyProfile">
                                 <h4>Beatriz Abne</h4>
                                 <h5>Web Developer</h5>
-// descrição e link de redes sociais (LinkedIn, Git e outros)
+
+// Descrição aluno 1
                                 <ul className = "nav nav-tabs" role = "tablist">
                                     <li className = "nav-item">
                                     <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
@@ -24,10 +26,143 @@ const AboutUs = () => {
                                     <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
                                     </li>
                                 </ul>
-
                             </div>
                         </div>
+                    </div>
+                </form>
+            </div>
 
+// Card foto Aluno 2
+            <div className = "profileTeam">
+                <form method = "">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <img src = {dominic} alt="Abne"/>
+                        </div>
+                        <div className = "col-md-6">
+                            <div className = "bodyProfile">
+                                <h4>Dominic</h4>
+                                <h5>Web Developer</h5>
+
+// Descrição aluno 2
+                                <ul className = "nav nav-tabs" role = "tablist">
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
+                                    </li>
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+// Card foto Aluno 3
+            <div className = "profileTeam">
+                <form method = "">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <img src = {eduardo} alt="Abne"/>
+                        </div>
+                        <div className = "col-md-6">
+                            <div className = "bodyProfile">
+                                <h4>Eduardo</h4>
+                                <h5>Web Developer</h5>
+
+// Descrição aluno 3
+                                <ul className = "nav nav-tabs" role = "tablist">
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
+                                    </li>
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+// Card foto Aluno 4
+            <div className = "profileTeam">
+                <form method = "">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <img src = {lucas} alt="Abne"/>
+                        </div>
+                        <div className = "col-md-6">
+                            <div className = "bodyProfile">
+                                <h4>Lucas</h4>
+                                <h5>Web Developer</h5>
+
+// Descrição aluno 4
+                                <ul className = "nav nav-tabs" role = "tablist">
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
+                                    </li>
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+// Card foto Aluno 5
+            <div className = "profileTeam">
+                <form method = "">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <img src = {murilo} alt="Abne"/>
+                        </div>
+                        <div className = "col-md-6">
+                            <div className = "bodyProfile">
+                                <h4>Murilo</h4>
+                                <h5>Web Developer</h5>
+
+// Descrição aluno 5
+                                <ul className = "nav nav-tabs" role = "tablist">
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
+                                    </li>
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+// Card foto Aluno 6
+            <div className = "profileTeam">
+                <form method = "">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <img src = {rafaela} alt="Abne"/>
+                        </div>
+                        <div className = "col-md-6">
+                            <div className = "bodyProfile">
+                                <h4>Rafaela</h4>
+                                <h5>Web Developer</h5>
+
+// Descrição aluno 6
+                                <ul className = "nav nav-tabs" role = "tablist">
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
+                                    </li>
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
