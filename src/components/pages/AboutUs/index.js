bf6e85887e3ef8e1b2583169985c5f94b328b41d
Murilo Cesar "Muca <85465611+MuriloMuca@users.noreply.github.com>
2021-12-06 23:22:02 -0300

    About Us parcial
    primeiros rascunhos da página About Us

---
+++ src/components/pages/AboutUs/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+// import imagem from "../AboutUs"
+
+const AboutUs = () => {
+    return (
+        <>
+// foto do perfil
+            <div className = "profileTeam">
+                <form method = "">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <img src = {abne} alt="Abne">
+                        </div>
+                        <div className = "col-md-6">
+                            <div className = "bodyProfile">
+                                <h4>Beatriz Abne</h4>
+                                <h5>Web Developer</h5>
+// descrição e link de redes sociais (LinkedIn, Git e outros)
+                                <ul className = "nav nav-tabs" role = "tablist">
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "descricao-tab" data-toggle="tab" href="#descricao" role="tab">Sobre</a>
+                                    </li>
+                                    <li className = "nav-item">
+                                    <a className = "nav-link active" id = "contatos-tab" data-toggle="tab" href="#contatos" role="tab">Contato</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+
+        </>
+    )
+}
+
+export default AboutUs
