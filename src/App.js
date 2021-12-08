37345745b57889bbec626585bb596b56b8b358b3
Eduardo Marcondes <eduardocmarcondes@gmail.com>
2021-12-08 20:58:36 -0300

    subiu banner e correção carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Home from './components/pages/Home';
+import RouteList from './routes';
 
 function App() {
   return (
     <>
-      <Home />
+      <RouteList />
     </>
   );
 }
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,25 +1,19 @@
 import React from "react";
 import './style.scss'
-import { Row, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 const Banner = () => {
 
     return (
 
         <section className="banner">
-            {/* <div className="row"> */}
-                <Row>
                 <div className="col-sm-6">
                     <Col>
                     <h2>Bem vindo ao UaiTatu!</h2>
                     <p>Aqui você conhecerá mais de nosso lindo projeto, onde, através do mundo das tatuagens, as pessoas podem se sentir mais livres e autênticas.</p>
                     </Col>
                 </div>
-                </Row>
-
-            {/* </div> */}
-
-        </section>
+     </section>
 
     )
 
