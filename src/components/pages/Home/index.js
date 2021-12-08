5dd747cfc536f0e1626472a15c38c6a2106bd352
Eduardo Marcondes <eduardocmarcondes@gmail.com>
2021-12-07 22:04:59 -0300

    subiu alterações composição carousel

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,80 +1,79 @@
-import React from "react";
+import {React, useState, useContext } from 'react';
 import { Carousel, Row, Col } from "react-bootstrap";
 import CarItem from "../../Carousel/carItem";
 
-export default class Home extends React.Component {
-    render() {
-        return (
-            <main>
-                <Carousel>
-                    <Carousel.Item >
-                        <Col lg={{ span: 6, offset: 4 }}>
-                        <Row>
-                        {this.state.itensCarousel1.map(
-                            (carItem) => {
-                                return (
-                                    <CarItem className="item"  itemSrc={carItem.itemSrc} itemAlt={carItem.itemAlt} key={carItem.id} />
-                                );
-                            }
-                        )}
-                        </Row>
-                        </Col>
-                    </Carousel.Item>
-                    <Carousel.Item >
-                        <Col lg={{ span: 6, offset: 4 }}>
-                        <Row>
-                        {this.state.itensCarousel2.map(
-                            (carItem) => {
-                                return (
-                                    <CarItem className="item"  itemSrc={carItem.itemSrc} itemAlt={carItem.itemAlt} key={carItem.id} />
-                                );
-                            }
-                        )}
-                        </Row>
-                        </Col>
-                    </Carousel.Item>
-                </Carousel>
-            </main>
-        );
-    }
+const Home = () => {
 
-    constructor() {
-        super();
-        this.state = {
-            itensCarousel1: [
-                {
-                    id: 1,
-                    itemSrc: "https://images.emojiterra.com/google/android-10/512px/1f7ea.png",
-                    itemAlt: "um quadrado roxo"
-                },
-                {
-                    id: 2,
-                    itemSrc: "https://images.emojiterra.com/google/android-10/512px/1f7ea.png",
-                    itemAlt: "segundo quadrado roxo"
-                },
-                {
-                    id: 3,
-                    itemSrc: "https://images.emojiterra.com/google/android-10/512px/1f7ea.png",
-                    itemAlt: "terceiro quadrado roxo"
-                }
-            ],            
-            itensCarousel2: [
-                {
-                    id: 1,
-                    itemSrc: "https://image.freepik.com/vetores-gratis/cartaz-vermelho-da-festa-de-feliz-natal-com-decoracao-realista_1393-455.jpg",
-                    itemAlt: "um quadrado roxo"
-                },
-                {
-                    id: 2,
-                    itemSrc: "https://image.freepik.com/vetores-gratis/cartaz-vermelho-da-festa-de-feliz-natal-com-decoracao-realista_1393-455.jpg",
-                    itemAlt: "segundo quadrado roxo"
-                },
-                {
-                    id: 3,
-                    itemSrc: "https://image.freepik.com/vetores-gratis/cartaz-vermelho-da-festa-de-feliz-natal-com-decoracao-realista_1393-455.jpg",
-                    itemAlt: "terceiro quadrado roxo"
-                }
-            ]
-        }
-    }
+    const [carItem] = useState([]);
+
+    return (
+        <main>
+            <Carousel>
+                <Carousel.Item >
+                    <Col lg={{ span: 6, offset: 4 }}>
+                        <Row>
+                            {itensCarousel1.map(({id, itemAlt, itemSrc}) => {
+                                    return (
+                                        <CarItem className="item" itemSrc={itemSrc} itemAlt={itemAlt} key={id} />
+                                    );
+                                }
+                            )}
+                        </Row>
+                    </Col>
+                </Carousel.Item>
+                <Carousel.Item >
+                    <Col lg={{ span: 6, offset: 4 }}>
+                        <Row>
+                            {itensCarousel2.map(({id, itemAlt, itemSrc}) => {
+                                    return (
+                                        <CarItem className="item" itemSrc={itemSrc} itemAlt={itemAlt} key={id} />
+                                    );
+                                }
+                            )}
+                        </Row>
+                    </Col>
+                </Carousel.Item>
+            </Carousel>
+        </main>
+    );
 }
+
+// transformar no consumo da api do Backend CTD-Commerce
+const itensCarousel1 = [
+            {
+                id: 1,
+                itemSrc: "https://images.emojiterra.com/google/android-10/512px/1f7ea.png",
+                itemAlt: "um quadrado roxo"
+            },
+            {
+                id: 2,
+                itemSrc: "https://images.emojiterra.com/google/android-10/512px/1f7ea.png",
+                itemAlt: "segundo quadrado roxo"
+            },
+            {
+                id: 3,
+                itemSrc: "https://images.emojiterra.com/google/android-10/512px/1f7ea.png",
+                itemAlt: "terceiro quadrado roxo"
+            }
+        ];
+        
+        const itensCarousel2 = [
+            {
+                id: 1,
+                itemSrc: "https://image.freepik.com/vetores-gratis/cartaz-vermelho-da-festa-de-feliz-natal-com-decoracao-realista_1393-455.jpg",
+                itemAlt: "um quadrado roxo"
+            },
+            {
+                id: 2,
+                itemSrc: "https://image.freepik.com/vetores-gratis/cartaz-vermelho-da-festa-de-feliz-natal-com-decoracao-realista_1393-455.jpg",
+                itemAlt: "segundo quadrado roxo"
+            },
+            {
+                id: 3,
+                itemSrc: "https://image.freepik.com/vetores-gratis/cartaz-vermelho-da-festa-de-feliz-natal-com-decoracao-realista_1393-455.jpg",
+                itemAlt: "terceiro quadrado roxo"
+            }
+        ]
+    
+
+export default Home;
