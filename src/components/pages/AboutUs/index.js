5b5247bdb55fe5499eafab19bef654724bf86a0c
Murilo Cesar "Muca <85465611+MuriloMuca@users.noreply.github.com>
2021-12-09 12:30:43 -0300

    Atualizações AboutUs
    Cards formados e faltando interatividade  e responsividade

--- src/components/pages/AboutUs/index.js
+++ src/components/pages/AboutUs/index.js
@@ -1,125 +1,16 @@
-import React from "react-bootstrap";
-import img from "../AboutUs";
-import "./style.scss"
+import React from "react";
+import "./style.scss";
+import Header from "../../Header"
+import TeamCards from "../../TeamCards"
 
 
-const AboutUs=() => {
+
+const AboutUs= () => {
     return (
         <>
- {/* Card foto Aluno 1 */}
-
-            <div className="profileTeam">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src="{img}" alt="Abne"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="bodyProfile">
-                                <h3>Beatriz Abne</h3>
-                                <h5>Web Developer</h5>
-                                <button>LinkedIn</button>
-                                <button>GitHub</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-{/* Card foto Aluno 2 */}
-            <div className="profileTeam">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={img} alt="Dominic"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="bodyProfile">
-                                <h3>Dominic</h3>
-                                <h5>Web Developer</h5>
-                                <button>LinkedIn</button>
-                                <button>GitHub</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-{/* Card foto Aluno 3 */}
-            <div className="profileTeam">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={img} alt="Eduardo"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="bodyProfile">
-                                <h3>Eduardo</h3>
-                                <h5>Web Developer</h5>
-                                <button>LinkedIn</button>
-                                <button>GitHub</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-{/* Card foto Aluno 4 */}
-            <div className="profileTeam">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={img} alt="Lucas"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="bodyProfile">
-                                <h3>Lucas</h3>
-                                <h5>Web Developer</h5>
-                                <button>LinkedIn</button>
-                                <button>GitHub</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-{/* Card foto Aluno 5 */}
-            <div className="profileTeam">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={img} alt="Murilo"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="bodyProfile">
-                                <h3>Murilo</h3>
-                                <h5>Web Developer</h5>
-                                <button>LinkedIn</button>
-                                <button>GitHub</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-{/* Card foto Aluno 6 */}
-            <div className="profileTeam">
-                <form method="">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={img} alt="Rafaela"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="bodyProfile">
-                                <h3>Rafaela</h3>
-                                <h5>Web Developer</h5>
-                                <button>LinkedIn</button>
-                                <button>GitHub</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+        <Header/>
+        <TeamCards/>
+        <Footer/>
 
         </>
     )
